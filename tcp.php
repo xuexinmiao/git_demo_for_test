ebbf16bf20fefc760e2507735083fd2f59609156
xuexinmiao <742762281@qq.com>
2025-04-06 10:42:15 +0800

    0406

--- tcp.php
+++ tcp.php
@@ -2,6 +2,11 @@
 //创建Server对象，监听 127.0.0.1:9501 端口。
 $server = new Swoole\Server('127.0.0.1', 9501);
 
+$server->set([
+    'worker_num' => 1,
+    'task_worker_num' => 1,
+]);
+
 //监听连接进入事件。
 $server->on('Connect', function ($server, $fd) {
     echo "Client: Connect.\n";
@@ -9,6 +14,8 @@ $server->on('Connect', function ($server, $fd) {
 
 //监听数据接收事件。
 $server->on('Receive', function ($server, $fd, $reactor_id, $data) {
+    $server->send($fd, "aaaaaaaaaaaaaa");
+    sleep(10);
     $server->send($fd, "Server: {$data}");
 });
 
